92d212931b5f77241364395c2d339c6bc61de8ac
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-02 00:02:39 +0200

    add basic routes for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
+import { ViewsHomeComponent } from './views/views-home/views-home.component';
+import { ModsHomeComponent } from './mods/mods-home/mods-home.component';
 
 const routes: Routes = [
-  {path: 'elements', component: ElementsHomeComponent}
+  {path: 'elements', component: ElementsHomeComponent},
+  {path: 'collections', component: CollectionsHomeComponent},
+  {path: 'views', component: ViewsHomeComponent},
+  {path: 'mods', component: ModsHomeComponent}
+
+
 ];
 
 @NgModule({
