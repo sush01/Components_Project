6c69cedb9c87aa6d9fa3ba84c7161ef6810773db
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-02 01:40:39 +0200

    add placeholder component to element component

---
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: false,
+  templateUrl: './placeholder.component.html',
+  styleUrl: './placeholder.component.css'
+})
+export class PlaceholderComponent {
+
+}
