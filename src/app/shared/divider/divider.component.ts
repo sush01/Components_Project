3218f0536b2256c4471fca8e2fee39289592b1e4
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-02 12:04:35 +0200

    divider component working

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './divider.component.css'
 })
 export class DividerComponent {
+
+  @Input() title = '';
 
 }
