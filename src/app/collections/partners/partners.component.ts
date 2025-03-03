57ae00e412c8853e46b1e14047339ee90c1b6af6
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-03 22:18:33 +0200

    Create biography, ccompanies, partners component in collections folder for tab based navigation

---
+++ src/app/collections/partners/partners.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partners',
+  standalone: false,
+  templateUrl: './partners.component.html',
+  styleUrl: './partners.component.css'
+})
+export class PartnersComponent {
+
+}
