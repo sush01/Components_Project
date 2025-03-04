429e27e421f1355cb2f0d77c8c5de337ad301c39
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-04 16:42:32 +0200

    implement that the view module navigate to statistics component

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 
 @NgModule({
   declarations: [
-    ViewsHomeComponent
+    ViewsHomeComponent,
+    StatisticsComponent
   ],
   imports: [
     CommonModule,
