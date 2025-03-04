54fcc3ea56a8fcb47ec73ff424b3faa13a8a5435
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-04 23:55:55 +0200

    implement product display layout with image, tittle and description

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -6,12 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './views-home.component.html',
   styleUrl: './views-home.component.css'
 })
+
+
+
+
 export class ViewsHomeComponent {
 
   stats:{ value: number, label: string }[] = [
     {value: 22, label: '# of Users'},
     {value: 900, label: 'Revenue'},
     {value: 50, label: 'Reviews'}
-  ]
+  ];
+
+  items: {image: string, title:string, description:string} []=[
+    { image: '/assets/images/iphone.jpeg',
+      title: 'Iphone 16 Pro Max 5G',
+      description: 'This is a new  version of Iphone'
+    },
+    { image: '/assets/images/samsung.jpeg',
+      title: 'Samsung Galaxy S24 Ultra 5G',
+      description: 'This is a new Samsung Galaxy S24 Ultra 5G '
+    }
+  ];
 
 }
