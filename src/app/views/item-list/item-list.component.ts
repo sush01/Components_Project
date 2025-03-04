e553e244a60f8cc69f1449fe51abcae42e67ffdc
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-04 23:53:44 +0200

    add itemlist component to views module and add asset folder for images

---
+++ src/app/views/item-list/item-list.component.ts
@@ -0,0 +1,13 @@
+import { Component , Input} from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  standalone: false,
+  templateUrl: './item-list.component.html',
+  styleUrl: './item-list.component.css'
+})
+export class ItemListComponent {
+
+  @Input() items:{image: string, title:string, description:string} [] = [];
+
+}
