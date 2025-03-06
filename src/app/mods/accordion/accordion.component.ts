62bd855ee392664e0c00b80346ab108d2d3f9808
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-06 13:14:02 +0200

    create new component called Accordion in Mods module

---
+++ src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  standalone: false,
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.css'
+})
+export class AccordionComponent {
+
+}
