6e89a67316ca0c710eac8a5813ced22f7c56c007
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-06 13:46:11 +0200

    Implement function that let expand the active element in accordion and implement style in it

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -9,6 +9,16 @@ import { Component , Input} from '@angular/core';
 export class AccordionComponent {
 
   @Input() items:{title:string, content: string}[] = [];
- 
+  openedItemIndex = 0;
+
+onClick(index: number){
+  if (index === this.openedItemIndex){
+    this.openedItemIndex = -1;
+  } else{
+    this.openedItemIndex = index;
+  }
+  
+}
+
 
 }
